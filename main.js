f22023a3e790b5c83b1a73c3a5b6379d46213c14
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-06 16:49:20 +0200

    fine

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-let simbolo = document.querySelector('fas');
-console.log(simbolo);
-let menu = document.querySelector('hamburger-menu');
-console.log(menu);
 
+var menu = document.querySelector(".hamburger-menu");
+console.log(menu);
+var simbolo = document.querySelector(".fa-bars");
+console.log(simbolo);
+var chiusura = document.querySelector(".close");
+console.log(chiusura);
 
 simbolo.addEventListener ('click',
     function() {
         menu.style.display = 'block'
     }
 );
+
+chiusura.addEventListener ('click',
+    function() {
+        menu.style.display = 'none'
+    }
+);
+
 
