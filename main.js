803d9bac2a7a5f9ded868029e3a007cfbd220dc1
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-06 16:06:15 +0200

    working

---
+++ main.js
@@ -0,0 +1,14 @@
+var x = document.getElementsByClassName('fas');
+console.log(x);
+var y = document.getElementsByClassName('hamburger-menu');
+console.log(y);
+
+x.addEventListener ('click',
+
+function (){
+    y.style.display = 'block'
+}
+
+)
+
+
